4caefac5370bf1db872c922c98fc9906daf89f04
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-11-06 04:30:56 +0000

    added return negative

---
+++ Ruby/8kyu/return-negative.rb
@@ -0,0 +1,21 @@
+# In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+# Examples
+# makeNegative(1);  # return -1
+# makeNegative(-5); # return -5
+# makeNegative(0);  # return 0
+# Notes
+# The number can be negative already, in which case no change is required.
+# Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+# FUNDAMENTALS
+
+# My solution
+def make_negative(num)
+  num > 0 ? num * (-1) : num
+end
+
+# Other
+def makeNegative(num)
+  -num.abs
+end
