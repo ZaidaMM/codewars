86e329abefdd11f6281a35c6fc53fd009cfa3e39
ZaidaMM <75247321+ZaidaMM@users.noreply.github.com>
2022-11-08 12:54:08 +0000

    added are you square

---
+++ Ruby/7kyu/are-you-square.rb
@@ -0,0 +1,31 @@
+# DESCRIPTION:
+# Write a function that checks whether all elements in an array are square numbers. The function should be able to take any number of array elements.
+
+# Your function should return true if all elements in the array are square numbers and false if not.
+
+# An empty array should return undefined / None / nil /false (for C). You can assume that all array elements will be positive integers.
+
+# Examples:
+
+# is_square([1, 4, 9, 16]) --> true
+
+# is_square([3, 4, 7, 9]) --> false
+
+# is_square([]) --> nil
+# ARRAYS FUNDAMENTALS
+
+# My solution
+def is_square(arr)
+  return nil if arr == []
+  arr.all? {|x| Math.sqrt(x) % 1 == 0  }  
+end
+
+# Other
+def is_square(arr)
+  arr.length.positive? ? arr.all?{|x| Math.sqrt(x) % 1 == 0} : nil
+end
+
+
+def is_square(arr)
+  arr.empty? ? nil : arr.all? {|e| e**0.5%1==0}
+end
